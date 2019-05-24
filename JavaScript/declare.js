a6e1ae049cd86be10bc04722c9b22ff7e1104398
Jon Sibala <sibalajp@gmail.com>
2019-05-23 17:48:48 -0700

    updated declare.js

--- JavaScript/declare.js
+++ JavaScript/declare.js
@@ -1,4 +1,4 @@
-
+// Setting varibales
 
 var x = 10 
 var y = 12
@@ -7,3 +7,15 @@ var p = "pennylane"
 var greeting = "Hola" ;
 var greeting = "Yo" ;
 var goodbye = "Adios!" ;
+
+// Booleans
+var name = true;
+var x = false;
+
+if (name) {
+	console.log("This is true thus it's printing");
+}
+else if (x) {
+	console.log("***This should not print");
+}
+
