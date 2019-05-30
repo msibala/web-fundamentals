61dc1e3472d15eeedda7196c009039cf4bdf0fc5
msibala <miluchia.sibala@gmail.com>
2019-05-30 13:01:55 -0700

    added css, html

--- JavaScript/declare.js
+++ JavaScript/declare.js
@@ -21,4 +21,21 @@ else if (x) {
 else {
 	console.log("Not True or False")
 }
-
+var myName = 'Martin' ;
+console.log(myName) ;
+console.log("Hello World")
+var message = "Welcome to the Dojo"
+console.log(message) ;
+//decalre test take 3 
+var numDays = 40;
+console.log("It rained for " + numDays + " days and nights!") ;
+var greeting = "howdy" ;
+console.log("greeting " + greeting) ;
+var numFlags = 40;
+console.log("we need " + numFlags +" flags for the party")//number1 + string1
+var numGuests = 100 ; 
+console.log("There will be " +numGuests + " at the wedding")//number 2 + string 2
+var numDogs = 300 ; 
+console.log("The event has " + numDogs + " as entertainment")//number3 + string 3
+var numPlaylist = 21 ;
+console.log("Ian created " + numPlaylist + "  for the party that can also be used for the wedding.")//number4 + string 4
