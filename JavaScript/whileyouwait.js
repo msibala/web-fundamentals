a1d13919585ee758789ce2f947ab70c5473dafad
msibala <miluchia.sibala@gmail.com>
2019-06-10 22:02:28 -0700

    more on javascript

---
+++ JavaScript/whileyouwait.js
@@ -0,0 +1,19 @@
+var num = 60;                                      
+while (num > 0 )                                   
+{
+  console.log(num,"num")
+  if(num > 30) {
+    console.log("I'm sad")
+  }
+ 
+  if(num < 30) {
+      console.log("!!!!!")
+    if(num < 5) {
+        console.log("IT'S MY BIRTHDAY!")
+    }
+  }     
+                  
+  num = num - 1;
+}
+console.log("Happy Birthday");  
+
