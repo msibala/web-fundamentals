bd761efaea62cb873d4e560ffce370ecfcac684b
msibala <miluchia.sibala@gmail.com>
2019-06-11 21:47:15 -0700

    updated work with javascript

---
+++ JavaScript/idodeclare.js
@@ -0,0 +1,13 @@
+
+		
+		console.log("Hi, my name is Mia")
+		var message = "I'm doing a self teaching of code.";
+		console.log(message);
+		var numDays = 90;
+		console.log("It will go on for a total of  " + numDays + " days and nights!");
+		var numDays = 6;
+		console.log("For " + numDays + " days and nights a week.");
+		var numDays = 1;
+		console.log(" "+ numDays + " day to recharge.");
+		var itwillbechallengingbutfun = true;
+		var codingisLame = false;
