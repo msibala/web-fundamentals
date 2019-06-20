3face7288fe55764314c857431ba51ada3867d70
msibala <miluchia.sibala@gmail.com>
2019-06-19 19:48:46 -0700

    javascript fundamentals

---
+++ JavaScript/forabillion.js
@@ -0,0 +1,11 @@
+var earnings = .01;
+		var amount = .01;
+		for (var i = 2; i <= 100000; i++) {
+			earnings = earnings * 2
+			console.log("earnings", earnings)
+			amount = amount + earnings
+			console.log("amount", amount)
+			if(amount == infinity){
+				console.log(i)
+      }
+    } 
