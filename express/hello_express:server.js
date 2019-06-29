88ce7da275f491db7c0ab68d33a5ab4ea18e998d
msibala <miluchia.sibala@gmail.com>
2019-06-28 21:58:36 -0700

    practice with node and express

---
+++ express/hello_express:server.js
@@ -0,0 +1,1 @@
+hello_express/server.js
--- node_server/app.js
+++ node_server/app.js
@@ -3,12 +3,19 @@ var http = require('http');
 //fs module allows us to read and write content for responses!!
 var fs = require('fs');
 //creating a server using http module:
-var server = http.createServer(function (request, reponse){
+var server = http.createServer(function (request, response){
 	//see what URL the clients are requesting:
 	console.log('client request URL: ', request.url);
 	//this is how we do routing:
 	if(request.url === '/') {
 		fs.readFile('index.html', 'utf8', function (errors, contents){
+			response.writeHead(200, {'Content-Type' : 'text/html'}); //send data about response
+			response.write(contents); //send response body
+			response.end(); //finished!
+		});	
+	}
+	else if (request.url === '/dojo') {
+		fs.readFile('dojo.html', 'utf8', function (errors, contents){
 			response.writeHead(200, {'Content-Type' : 'text/html'}); //send data about response
 			response.write(contents); //send response body
 			response.end(); //finished!
